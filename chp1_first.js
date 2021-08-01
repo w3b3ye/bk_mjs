50c858d9ff4eb77505f8962cdab99248411223af
w3b3ye <w3b3ye@gmail.com>
2021-08-01 16:45:01 +0000

    Basic variable example.

---
+++ chp1_first.js
@@ -0,0 +1,15 @@
+let a = 6
+let b = 7
+let x
+
+console.log(a * b)
+console.log(typeof (a)) //Will print number.
+console.log(typeof (x)) //Will print undefined
+
+const notQuitePi = parseFloat('3.14')
+console.log("This is a float :", notQuitePi) //Print number 3.14
+
+const evenLessPi = parseInt('3.14')
+console.log("This is an integer :", evenLessPi) //Print number 3 because we parse it to integer.
+
+console.log("This is just a string :", notQuitePi.toString()) //Print 3.14 as string.
