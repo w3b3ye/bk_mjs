346af1a08dc9e2836d903ec58932a6f7f9243e23
w3b3ye <w3b3ye@gmail.com>
2021-08-01 21:13:06 +0000

    Minor update.

--- chp1_first.js
+++ chp1_first.js
@@ -8,8 +8,15 @@ console.log(typeof (x)) //Will print undefined
 
 const notQuitePi = parseFloat('3.14')
 console.log("This is a float :", notQuitePi) //Print number 3.14
-
 const evenLessPi = parseInt('3.14')
-console.log("This is an integer :", evenLessPi) //Print number 3 because we parse it to integer.
 
+console.log("This is an integer :", evenLessPi) //Print number 3 because we parse it to integer.
 console.log("This is just a string :", notQuitePi.toString()) //Print 3.14 as string.
+console.log("This will return a NaN (Not a Number) :", 0 / 0) //Will return a NaN.
+
+let counter = 0
+let riddle = counter++
+console.log(riddle)
+
+let enigma = ++counter
+console.log(enigma)
