78cbb584f5628a469b04e4a9cc24b63d05d78b25
w3b3ye <w3b3ye@gmail.com>
2021-08-03 19:15:51 +0000

    Chapter 2 code.

---
+++ chp1_exr.js
@@ -0,0 +1,15 @@
+// 1 find out the outputs?
+console.log(0 + NaN)
+console.log(0 + Infinity)
+console.log(0 + false)
+console.log(0 + true)
+console.log(0 + null)
+console.log(0 + undefined)
+
+//2
+console.log('' + NaN)
+console.log('' + Infinity)
+console.log('' + false)
+console.log('' + true)
+console.log('' + null)
+console.log('' + undefined)
